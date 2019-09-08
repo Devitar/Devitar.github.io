7c0b8adfc3ffc1f3060f8d2a4794592088702608
Devitar <46533796+Devitar@users.noreply.github.com>
2019-09-08 12:26:53 -0600

    Revert "Revert "Updates""
    This reverts commit 54d5f1d485179e20a6521f2afc73e81864227994.

---
+++ precache-manifest.48e75dd830bd6e2ef17e2c6df6e52dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b950f6fe6022cd3223ba9c8b22fc07a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "face86aaf92b40da155d",
+    "url": "/static/css/main.adee51ff.chunk.css"
+  },
+  {
+    "revision": "d00f32e7ba2b014b7160",
+    "url": "/static/js/2.4b171ea1.chunk.js"
+  },
+  {
+    "revision": "face86aaf92b40da155d",
+    "url": "/static/js/main.03191c44.chunk.js"
+  },
+  {
+    "revision": "d369e20d60180c32392a",
+    "url": "/static/js/runtime~main.34d2fea7.js"
+  }
+]);
