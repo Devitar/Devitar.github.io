54d5f1d485179e20a6521f2afc73e81864227994
Devitar <46533796+Devitar@users.noreply.github.com>
2019-09-08 12:24:39 -0600

    Revert "Updates"
    This reverts commit f8aa595073917932ac1046ca9d1210d3e6c3c9dd.

---
+++ src/components/mainMenu.tsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import '../index.css';
+
+//components
+//import ContactMe from './contactMe';
+
+// const pageComponents: {[index: string]: any} = {
+//     'Contact': ContactMe,
+// };
+
+export default class MainMenu extends Component {
+    state = { activeItem: 'Portfolio' }
+
+    handleItemClick = (e: any, { name }: any) => {
+        this.setState({ activeItem: name }) 
+    }
+
+    render() {
+        const activeItem: string = this.state.activeItem;
+        // const activeContent = pageComponents[activeItem];
+
+        return (
+        <div>
+
+            <div className='mainHeader'>
+                Devin Curtis
+            </div>
+
+            <Menu pointing secondary>
+            <Menu.Item
+                name='Portfolio'
+                active={activeItem === 'Portfolio'}
+                onClick={this.handleItemClick}
+            />
+            <Menu.Item
+                name='Resume'
+                active={activeItem === 'Resume'}
+                onClick={this.handleItemClick}
+            />
+            <Menu.Item
+                name='Contact'
+                active={activeItem === 'Contact'}
+                onClick={this.handleItemClick}
+            />
+            </Menu>
+
+            <div className='subHeader'>
+                <p>This site is a work in progess. Some things are not yet functional. Sorry!</p>
+            </div>
+            <div className='subHeader'>
+                <a href='https://github.com/Devitar'>My GitHub!</a>
+            </div>
+
+            <div className='pageContent'>
+                <p className="portfolioLink">My Portfolio Projects</p>
+                <a href='https://devincurtis.me/E-commerce-Store/' className='portfolioLink'>E-Commerce Store (React)</a>
+                <a href='https://dcurtis-user-manager-postgres.herokuapp.com' className='portfolioLink'>User Manager (PostgreSQL)</a>
+                <a href='https://devincurtis.me/ToDoApp/' className='portfolioLink'>To Do App (jQuery + Bootstrap)</a>
+                <a href='https://angularfire-example-b425d.firebaseapp.com/' className='portfolioLink'>Company Manager (Angular)(WIP)</a>
+            </div>
+
+        </div>
+        )
+    }
+}
