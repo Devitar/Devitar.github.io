6b62b520d0f1549bbdc6ca0e76366beb8cbd6e31
Devin Curtis <devin.curtis1210@gmail.com>
2026-01-29 16:42:45 -0700

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
